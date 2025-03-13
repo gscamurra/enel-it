4f0ce0bf712861364b473c675525f9aaafc4d818
gscamurra <giovanni.scamurra.adobe@gmail.com>
2025-03-13 16:27:41 +0100

    Update alert.js

--- blocks/alert/alert.js
+++ blocks/alert/alert.js
@@ -2,10 +2,10 @@ export default function decorate(block) {
     
     const alertMessage = block.textContent.trim();
     const divAlert = document.createElement("div");
-
-    divAlert.innerHTML = "<p>" + alertMessage + "</p>";    
     
+    divAlert.innerHTML = "<p>" + alertMessage + "</p>";     
     block.replaceWith(divAlert);
+    block.classList.add("alert-box");
 
     console.log("Alert message: " + divAlert);
 
