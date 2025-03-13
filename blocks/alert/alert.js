7cc0276b638ba6b17c2528e92365707624065db8
gscamurra <giovanni.scamurra.adobe@gmail.com>
2025-03-13 21:28:29 +0100

    Update alert.js

--- blocks/alert/alert.js
+++ blocks/alert/alert.js
@@ -4,7 +4,6 @@ export default function decorate(block) {
     const pAlert = document.createElement("p");
 
     pAlert.innerHTML = alertMessage + "<a id=\"CloseLink\">X</a>";
-    console.log("Node: " + block.children[0].val());
     block.append(pAlert);
     
     document.getElementById("CloseLink").onclick = CloseAlert;    
