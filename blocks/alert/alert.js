c8176508665a0576e3c1e0e6d1b1cebc0f41338e
gscamurra <giovanni.scamurra.adobe@gmail.com>
2025-03-13 17:32:50 +0100

    Update alert.js

--- blocks/alert/alert.js
+++ blocks/alert/alert.js
@@ -1,16 +1,13 @@
 export default function decorate(block) {
     
-    window.addEventListener("load", function(){
-        document.getElementById("CloseLink").onclick = function() { 
-            console.log("Click alert");
-        };
-   });
-
-
     const alertMessage = block.textContent.trim();
     const divAlert = document.createElement("div");
+
+    const alertLink = document.createElement('a');
+    alertLink.setAttribute("onclick","CloseMessage()");
+    alertLink.appendChild(document.createTextNode("This is link"));
     
-    divAlert.innerHTML = "<p>" + alertMessage + "<a id=\"CloseLink\">Ok, chiudi finestra</a></p>";     
+    divAlert.innerHTML = "<p>" + alertMessage + "<a id=\"CloseLink\">Ok, chiudi finestra</a></p>" + alertLink;     
     block.replaceWith(divAlert);
     
     console.log("Alert message: " + divAlert);
