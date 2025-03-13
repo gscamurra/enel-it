2822999b9bee163fa0cc6815973d5d93517593b4
gscamurra <giovanni.scamurra.adobe@gmail.com>
2025-03-13 17:34:42 +0100

    Update alert.js

--- blocks/alert/alert.js
+++ blocks/alert/alert.js
@@ -2,12 +2,8 @@ export default function decorate(block) {
     
     const alertMessage = block.textContent.trim();
     const divAlert = document.createElement("div");
-
-    const alertLink = document.createElement('a');
-    alertLink.setAttribute("onclick","CloseMessage()");
-    alertLink.appendChild(document.createTextNode("This is link"));
     
-    divAlert.innerHTML = "<p>" + alertMessage + "<a id=\"CloseLink\">Ok, chiudi finestra</a></p>" + alertLink;     
+    divAlert.innerHTML = "<p>" + alertMessage + "<a id=\"CloseLink\">Ok, chiudi finestra</a></p>";     
     block.replaceWith(divAlert);
     
     console.log("Alert message: " + divAlert);
