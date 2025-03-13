870d9872e7e3dd2374e601e570dab38c73bfa61d
gscamurra <giovanni.scamurra.adobe@gmail.com>
2025-03-13 21:21:19 +0100

    Update alert.js

--- blocks/alert/alert.js
+++ blocks/alert/alert.js
@@ -5,7 +5,7 @@ export default function decorate(block) {
 
     //divAlert.innerHTML = "<p>" + alertMessage + "<a id=\"CloseLink\">X</a></p>";
     
-    console.log("Node: " + block.children[0]);
+    console.log("Node: " + block.children[0].val());
     //console.log(block.childNodes);
     
     //block.replaceWith(divAlert);
