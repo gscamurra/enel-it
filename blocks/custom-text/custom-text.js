4de60cc07df680e71dcacd281e790026d8b49cae
Alessio Giorgini <alessio.giorgini@gmail.com>
2025-04-10 00:33:26 +0200

    custom text component

---
+++ blocks/custom-text/custom-text.js
@@ -0,0 +1,8 @@
+export default async function decorate(block) {
+    // Prende il primo paragrafo e lo inserisce nel blocco
+    const text = block.textContent;
+    block.innerHTML = '';
+    const p = document.createElement('p');
+    p.textContent = text;
+    block.appendChild(p);
+  }
